c431959630542d075f4ac92eca55224452f5ae51
Bateyjosue <josuebatey19@gmail.com>
2023-10-01 20:37:09 +0200

    project setup react with vite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <>
+      <main className="bg-black/90 h-[100vh] text-white flex justify-center items-center">
+        <h1 className="text-6xl">React Calculator</h1>
+      </main>
+    </>
+  )
+}
+
+export default App
